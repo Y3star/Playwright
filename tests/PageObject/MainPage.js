0c1649eb757c972a35a2240e5f7ef03962372a09
Y3star <117822700+Y3star@users.noreply.github.com>
2022-12-07 17:21:47 +0200

    up

--- tests/PageObject/MainPage.js
+++ tests/PageObject/MainPage.js
@@ -13,19 +13,62 @@ function ganerateSrting(length) {
 }
 
 exports.MainPage = class MainPage {
+    constructor(page) {
+        this.page = page;
+        this.inputSearchInput = page.locator('[id="q"]');
+        //  #main-menu Link's (button's)
+        this.reviewLink = page.locator('[href="/projects/redmine"]'); //Обзор
+        this.downloadLink = page.locator(
+            '#main-menu [href*="/projects/redmine/wiki/Download"]'
+        ); //Download
+        this.activityLink = page.locator('[href="/projects/redmine/activity"]'); //Действия
+        this.roadmapLink = page.locator('[href="/projects/redmine/roadmap"]'); //Оператывний план
+        this.tasksLink = page.locator('[href="/projects/redmine/issues"]'); //Задачи
+        this.newsLink = page.locator('[href="/projects/redmine/news"]'); //Новости
+        this.wikiLink = page.locator('[href="/projects/redmine/wiki"]'); //Wiki
+        this.forumsLink = page.locator('[href="/projects/redmine/boards"]'); //Форумы
+        this.repositoryLink = page.locator(
+            '[href="/projects/redmine/repository"]'
+        ); //Repository
+        //
+    }
 
-  constructor(page) {
-    this.page = page;
-    this.inputSearchInput = page.locator('[id="q"]')  
+    async goto() {
+        await this.page.goto("https://www.redmine.org/");
+    }
+    //
+    async clickReview() {
+        await this.reviewLink.click();
+    }
+    async clickDownload() {
+        await this.downloadLink.click();
+    }
+    async clickActivity() {
+        await this.activityLink.click();
+    }
+    async clickRoadmap() {
+        await this.roadmapLink.click();
+    }
+    async clickTasks() {
+        await this.tasksLink.click();
+    }
+    async clickNews() {
+        await this.newsLink.click();
+    }
+    async clickWiki() {
+        await this.wikiLink.click();
+    }
 
-  }
+    async clickForums() {
+        await this.forumsLink.click();
+    }
 
-  async goto() {
-    await this.page.goto('https://www.redmine.org/');
-  }
+    async clickRepository() {
+        await this.repositoryLink.click();
+    }
+    //
 
-  async ganerateText(){
-    await this.inputSearchInput.fill(ganerateSrting(15))
-  }
-
-}
+    async ganerateText() {
+        await this.inputSearchInput.fill(ganerateSrting(15));
+    }
+};
--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,32 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const { MainPage } = require('../tests/PageObject/MainPage');
-let mainPage;
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms)) // only create delay to see something in test
+let mainPage;
 
-test('homepage has title and input to intro page', async ({ page }) => {
+test('Homepage has title and input to intro page', async ({ page }) => {
   mainPage = new MainPage(page);
   await mainPage.goto()
+
+  await mainPage.clickReview()
+  await delay(250);
+  await mainPage.clickDownload()
+  await delay(250);
+  await mainPage.clickActivity()
+  await delay(250);
+  await mainPage.clickRoadmap()
+  await delay(250);
+  await mainPage.clickTasks()
+  await delay(250);
+  await mainPage.clickNews()
+  await delay(250);
+  await mainPage.clickWiki()
+  await delay(250);
+  await mainPage.clickForums()
+  await delay(250);
+  await mainPage.clickRepository()
+  await delay(250);
+
   await mainPage.ganerateText()
   await delay(1000);
-
-
 });
