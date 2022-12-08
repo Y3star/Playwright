9f9c5ccd601af9b32b5a19e868789821e6598124
Y3star <117822700+Y3star@users.noreply.github.com>
2022-12-08 13:13:51 +0200

    up

--- tests/test.js
+++ tests/test.js
@@ -3,53 +3,53 @@ const { test, expect } = require("@playwright/test");
 const { MainPage } = require("../tests/PageObject/MainPage");
 const { WikiPage } = require("../tests/PageObject/WikiPage");
 const delay = (ms) => new Promise((a) => setTimeout(a, ms)); // only create delay to see something in test
+test.use({ viewport: { width: 1920, height: 1080 } }); // local config for size window
+test.use({ locale: "en-GB" });
 let mainPage;
 let wikiPage;
 
+test.describe("Test suit 1", () => {
+    test("Homepage has main menu to intro page", async ({ page }) => {
+        mainPage = new MainPage(page);
+        wikiPage = new WikiPage(page);
 
+        await mainPage.goto();
 
-test("Homepage has title and input to intro page", async ({ page }) => {
-    mainPage = new MainPage(page);
-    wikiPage = new WikiPage(page);
+        await mainPage.clickReviewNavTab(mainPage.reviewLink);
+        await delay(250);
 
-    await mainPage.goto();
+        await mainPage.clickDownload();
+        await delay(250);
+        await mainPage.clickActivity();
+        await delay(250);
+        await mainPage.clickRoadmap();
+        await delay(250);
+        await mainPage.clickTasks();
+        await delay(250);
+        await mainPage.clickNews();
+        await delay(250);
+        await mainPage.clickWiki();
+        await delay(250);
+        await mainPage.clickForums();
+        await delay(250);
+        await mainPage.clickRepository();
+        await delay(250);
 
-    await mainPage.clickReviewNavTab(mainPage.reviewLink);
-    await delay(250);
+        await mainPage.ganerateText();
+        await delay(1000);
 
-    await mainPage.clickDownload();
-    await delay(250);
-    await mainPage.clickActivity();
-    await delay(250);
-    await mainPage.clickRoadmap();
-    await delay(250);
-    await mainPage.clickTasks();
-    await delay(250);
-    await mainPage.clickNews();
-    await delay(250);
-    await mainPage.clickWiki();
-    await delay(250);
-    await mainPage.clickForums();
-    await delay(250);
-    await mainPage.clickRepository();
-    await delay(250);
+        await mainPage.clickWiki();
+        await delay(250);
 
-    await mainPage.ganerateText();
-    await delay(1000);
-
-    await mainPage.clickWiki();
-    await delay(250);
-
-    await expect(wikiPage.redmineList).toHaveText([
-        "Redmine",
-        "Features",
-        "Documentation",
-        "Online demo",
-        "Support & getting help",
-        "Contributing and helping out",
-        "Who uses Redmine?",
-        "Redmine books"
-    ]);
-
-    
+        await expect(wikiPage.redmineList).toHaveText([
+            "Redmine",
+            "Features",
+            "Documentation",
+            "Online demo",
+            "Support & getting help",
+            "Contributing and helping out",
+            "Who uses Redmine?",
+            "Redmine books",
+        ]);
+    });
 });
