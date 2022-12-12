fd0f62e6933d546d615788cf62d1e623b988f16e
Y3star <117822700+Y3star@users.noreply.github.com>
2022-12-12 14:21:30 +0200

    up

---
+++ tests/fifth.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+const { MainPage } = require("../tests/PageObject/MainPage");
+const { WikiPage } = require("../tests/PageObject/WikiPage");
+test.use({ viewport: { width: 1920, height: 1080 } }); // local config for size window
+test.use({ locale: "en-GB" }); // local config for eng Page // need for assert
+let page;
+let mainPage;
+let wikiPage;
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    mainPage = new MainPage(page);
+    wikiPage = new WikiPage(page);
+});
+
+test("Assert ul , and written text in search input", async () => {
+    
+});
