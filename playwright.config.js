f84b99dd50fad3478bf45e13ec58d0f597aa5c06
Y3star <117822700+Y3star@users.noreply.github.com>
2022-12-14 08:56:22 +0200

    up

--- playwright.config.js
+++ playwright.config.js
@@ -28,9 +28,9 @@ const config = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html',{open: 'never'}],
@@ -39,13 +39,11 @@ const config = {
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: process.env.CI ? "retain-on-failure" : "on",
+    screenshot: process.env.CI ? "only-on-failure" : "on",
+    video: process.env.CI ? 'retain-on-failure' : 'on',
+    ignoreHTTPSErrors: true,
   },
 
   /* Configure projects for major browsers */
